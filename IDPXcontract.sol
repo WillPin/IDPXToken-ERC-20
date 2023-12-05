7fa016e4f2f078149d5dc7d653de1bc45b9ca738
Will <115800499+WillPin@users.noreply.github.com>
2023-12-05 12:38:49 -0300

    Update IDPXcontract.sol

--- IDPXcontract.sol
+++ IDPXcontract.sol
@@ -1,78 +1,100 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-contract IDPXTokencontract {
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    // Detalhes do Token
-    address public owner;
-    string public name = "IDPXToken";
-    string public symbol = "IDPX";
-    uint8 public decimals = 4;
-    uint256 private _totalSupply = 1000000;
+contract IDPXTokencontract is Ownable {
 
-    // Mapiando
-    mapping (address => uint256) _balances;
-    mapping (address => mapping(address => uint256)) _allowed;
+  // Detalhes do Token
+  string public name = "IDPXToken";
+  string public symbol = "IDPX";
+  uint8 public decimals = 4;
+  uint256 private _totalSupply = 1000000;
 
-    // Eventos
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+  // Mapiando
+  mapping(address => uint256) private _balances;
+  mapping(address => mapping(address => uint256)) private _allowed;
+  mapping(address => bool) public _blacklist;
 
-    // Construção
-    constructor(){
-        owner = msg.sender;
-        _balances[msg.sender] = _totalSupply;
-        emit Transfer(address(0), msg.sender, _totalSupply);
-    }
+  // Eventos
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+  event BlacklistUpdated(address indexed user, bool isBlacklisted);
 
-    // Funções
-    function mint(address to, uint256 amount) private  {
-        require(msg.sender == owner, "Only owner can mint tokens");
-        _totalSupply += amount;
-        _balances[to] += amount;
-        emit Transfer(address(0), to, amount);
-    }
+  // Construção
+  constructor() Ownable(msg.sender) {
+    _balances[msg.sender] = _totalSupply;
+    emit Transfer(address(0), msg.sender, _totalSupply);
+  }
 
-    function burn(uint256 amount) private {
-        require(_balances[msg.sender] >= amount, "Insufficient balance");
-        _totalSupply -= amount;
-        _balances[msg.sender] -= amount;
-        emit Transfer(msg.sender, address(0), amount);
-    }
+  // Funções
+  function mint(address to, uint256 amount) public onlyOwner {
+    _totalSupply += amount;
+    _balances[to] += amount;
+    emit Transfer(address(0), to, amount);
+  }
 
-    function totalSupply() public view returns (uint) {
-        return _totalSupply - _balances[address(0)];
-    }
+  function burn(uint256 amount) public {
+    require(_balances[msg.sender] >= amount, "Insufficient balance");
+    _totalSupply -= amount;
+    _balances[msg.sender] -= amount;
+    emit Transfer(msg.sender, address(0), amount);
+  }
 
-    function balanceOf(address _owner) public view returns (uint balance) {
-        return _balances[_owner];
-    }
+  function totalSupply() public view returns (uint256) {
+    return _totalSupply - _balances[address(0)];
+  }
 
-    function allowance(address _owner, address _spender) public view returns (uint remaining) {
-        return _allowed[_owner][_spender];
-    }
+  function balanceOf(address owner) public view returns (uint256 balance) {
+    return _balances[owner];
+  }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(_balances[msg.sender] >= _value,"value exceeds senders balance");
-        _balances[msg.sender] -= _value;
-        _balances[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
-        return true;
-    }
+  function allowance(address owner, address spender) public view returns (uint256 remaining) {
+    return _allowed[owner][spender];
+  }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        _allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
+  function approve(address spender, uint256 value) public returns (bool success) {
+    _allowed[msg.sender][spender] = value;
+    emit Approval(msg.sender, spender, value);
+    return true;
+  }
 
-    function transferfrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= _balances[_from],"Not enough balance");
-        require(_value <= _allowed[_from][msg.sender],"Not enough allowance");
-        _balances[_from] -= _value;
-        _balances[_to] += _value;
-        _allowed[_from][msg.sender] -= _value;
-        emit Transfer(_from, _to, _value);
-        return true;
-    }
+  function transfer(address to, uint256 value) public returns (bool success) {
+    require(_balances[msg.sender] >= value, "value exceeds senders balance");
+    require(!_blacklist[to], "Recipient is blacklisted");
+
+    _balances[msg.sender] -= value;
+    _balances[to] += value;
+    emit Transfer(msg.sender, to, value);
+    return true;
+  }
+
+  function transferFrom(
+    address from,
+    address to,
+    uint256 value
+  ) public returns (bool success) {
+    require(value <= _balances[from], "Not enough balance");
+    require(value <= _allowed[from][msg.sender], "Not enough allowance");
+    require(!_blacklist[to], "Recipient is blacklisted");
+
+    _balances[from] -= value;
+    _balances[to] += value;
+    _allowed[from][msg.sender] -= value;
+    emit Transfer(from, to, value);
+    return true;
+  }
+
+  function addToBlacklist(address user) public onlyOwner {
+    require(!_blacklist[user], "User already blacklisted");
+    _blacklist[user] = true;
+    emit BlacklistUpdated(user, true);
+  }
+
+  function removeFromBlacklist(address user) public onlyOwner {
+    require(_blacklist[user], "User not blacklisted");
+    _blacklist[user] = false;
+    emit BlacklistUpdated(user, false);
+  }
+
 }
